027c6145d2cc5771b61dff75d63bd9ec3cea8127
reignyday <33501252+reignyday@users.noreply.github.com>
2021-12-13 18:28:29 -0400

    Create User.h

---
+++ User.h
@@ -0,0 +1,50 @@
+#ifndef __USER_H_
+#define __USER_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include "VoteNode.h"
+
+using namespace std;
+
+class User{
+    public:
+        void update_votes(int num_votes){
+            vote_count += num_votes;
+        }
+
+        User(){};
+        User(string ui){
+            user_id = ui;
+        }
+        User(const User &u){
+            user_id = u.user_id;
+            vote_count = u.vote_count;
+            left_user = new User;
+            right_user = new User;
+            *left_user = *(u.left_user);
+            *right_user = *(u.right_user);
+        }
+        ~User(){
+            if(left_user != nullptr){
+                delete left_user;
+            }
+            if(right_user != nullptr){
+                delete right_user;
+            }
+        }
+
+    // private:
+        string user_id = "initial value";
+        int vote_count = 0;
+
+        User * left_user = nullptr; //left child
+        User * right_user = nullptr; //right child
+
+        VoteNode * votepointer = nullptr;
+
+    friend class UserTree;   
+};
+
+#endif
